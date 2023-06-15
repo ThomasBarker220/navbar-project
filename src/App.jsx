91c14948c14b2f3eb531478d3885a7a30dc47e79
ThomasBarker220 <james.barker@duke.edu>
2023-06-14 22:10:20 -0400

    Initial comit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Example from "./Example";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <Example /> */}
+    </div>
+  );
+};
+export default App;
---
+++ src/Example.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from "react";
+import { links, social } from "./data";
+import { FaBars } from "react-icons/fa";
+import logo from "./logo.svg";
+
+const Example = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+
+        <ul className="links" ref={linksRef} style={linkStyles}>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Example;
